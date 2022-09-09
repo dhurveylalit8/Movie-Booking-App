658e8e84add66d2637c5e54f7d6d425272bce9b9
LalitDhurvey <dhurvelalit8@gmail.com>
2022-09-09 12:19:00 +0530

    added movie route code

---
+++ models/movie.model.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const constants = require("../utils/constant")
+
+const movieSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    casts : {
+        type : [String],
+        required : true
+    },
+    trailerUrls : {
+        type : [String],
+        required : true
+    },
+    posterUrls : {
+        type : [String],
+        required : true
+    },
+    languages : {
+        type : [String],
+        required : true
+    },
+    releaseDate : {
+        type : Date
+    },
+    releaseStatuses : {
+        type : String,
+        required : true,
+        enum : [constants.movieReleaseStatuses.released, constants.movieReleaseStatuses.coming_soon, constants.movieReleaseStatuses.blocked]
+    },
+    imdbRating : {
+        type : Number
+    },
+    genre : {
+        type : [String],
+        enum : [constants.movieGenre.action, constants.movieGenre.comedy, constants.movieGenre.drama, constants.movieGenre.fantasy, constants.movieGenre.horror, constants.movieGenre.mystery, constants.movieGenre.romance, constants.movieGenre.thriller]
+    }
+},{
+    timestamps : true,
+    versionKey : false
+});
+
+module.exports = mongoose.model("Movie", movieSchema)
---
+++ utils/constant.js
@@ -0,0 +1,26 @@
+module.exports = {
+    movieReleaseStatuses : {
+        released : "RELEASED",
+        coming_soon : "COMING_SOON",
+        blocked : "BLOCKED"
+    },
+
+    movieGenre : {
+        action : "ACTION",
+        comedy : "COMEDY",
+        drama : "DRAMA",
+        fantasy : "FANTASY",
+        horror : "HORROR",
+        mystery : "MYSTERY",
+        romance : "ROMANCE",
+        thriller : "THRILLER"
+    },
+
+    theatreShows : {
+        morning : "MORNING",
+        noon : "NOON",
+        evening : "EVENING",
+        night : "NIGHT"
+    }
+    
+}
