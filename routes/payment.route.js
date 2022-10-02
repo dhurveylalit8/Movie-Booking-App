b1f45e1b4581a0a0c30de48f57d8d17a7489a224
LalitDhurvey <dhurvelalit8@gmail.com>
2022-10-02 11:13:52 +0530

    checking readme.md

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -1,9 +1,9 @@
 const paymentController = require("../controllers/payment.controller");
-const {authJwt, validateIdInParams, validatePaymentRequestBody} = require("../middlewares");
+const {authJwt, validateIdInParams, validatePaymentRequestBody, validatePaymentRequestbody} = require("../middlewares");
 
 module.exports = (app) => {
 
-    app.post("/mba/api/v1/payments", [authJwt.verifyToken, validatePaymentRequestBody.newPaymentBody], paymentController.createPayment);
+    app.post("/mba/api/v1/payments", [authJwt.verifyToken, validatePaymentRequestbody.newPaymentBody], paymentController.createPayment);
 
     app.get("/mba/api/v1/payments/:id", [authJwt.verifyToken, validateIdInParams.paymentInParams ,authJwt.isAdminOrOwnerOfPayment], paymentController.getOnePayment);
 
